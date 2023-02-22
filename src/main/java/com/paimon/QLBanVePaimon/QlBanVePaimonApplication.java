5f3a9acb259ecad6355f2a184b782533dbd787ac
RioRichard <68910204+RioRichard@users.noreply.github.com>
2023-02-22 20:57:02 +0700

    first commit

---
+++ src/main/java/com/paimon/QLBanVePaimon/QlBanVePaimonApplication.java
@@ -0,0 +1,13 @@
+package com.paimon.QLBanVePaimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlBanVePaimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlBanVePaimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paimon/QLBanVePaimon/QlBanVePaimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paimon.QLBanVePaimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlBanVePaimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
