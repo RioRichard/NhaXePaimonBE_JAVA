4e012bf839eb65bf09fe658563e9829c66a50c11
RioRichard <68910204+RioRichard@users.noreply.github.com>
2023-02-27 23:25:52 +0700

    Add post

--- src/main/java/com/paimon/QLBanVePaimon/controllers/ManangerController.java
+++ src/main/java/com/paimon/QLBanVePaimon/controllers/ManangerController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paimon.QLBanVePaimon.AppConstant;
+import com.paimon.QLBanVePaimon.Helper;
+import com.paimon.QLBanVePaimon.models.Manager;
 import com.paimon.QLBanVePaimon.services.ManagerService;
 import com.paimon.QLBanVePaimon.sideModels.ResponseHandler;
 
@@ -52,32 +56,20 @@ public class ManangerController {
 
         }
     }
+    @PostMapping
+    public ResponseEntity<Object> post(@RequestBody Manager manager) {
+        var res = managerService.add(manager);
+        return ResponseHandler.generateMessage("Tạo thành công", HttpStatus.OK, "managers", res);
+        
+    }
 
     // @GetMapping("/test")
-    // public Pagination testMethod(HttpServletRequest request) {
+    // public Object testMethod() {
 
-    //     List<String> testTing = new ArrayList<>();
-
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
-    //     testTing.add("test1");
+        
 
 
-    //     return new Pagination(request, 0, 2,testTing.size(), "testMethod");
+    //     return Helper.hash256("63fcd8afea76fd03d8bc25d7" + "12345");
     // }
     
 }
--- src/main/java/com/paimon/QLBanVePaimon/services/ManagerService.java
+++ src/main/java/com/paimon/QLBanVePaimon/services/ManagerService.java
@@ -2,10 +2,12 @@ package com.paimon.QLBanVePaimon.services;
 
 import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.paimon.QLBanVePaimon.Helper;
 import com.paimon.QLBanVePaimon.models.Manager;
 import com.paimon.QLBanVePaimon.repositories.ManagerRepository;
 import com.paimon.QLBanVePaimon.sideModels.ListData;
@@ -25,6 +27,15 @@ public class ManagerService {
 
     public Optional<Manager> getId(String id) {
         return managerRepository.findById(id);
+    }
+
+    public Manager add(Manager manager){
+        var id = new ObjectId();
+        manager.setId(id.toString());
+        var hasedPass = Helper.hash256(manager.getId()+manager.getPass());
+        manager.setPass(hasedPass);
+        System.out.println(hasedPass);
+        return managerRepository.insert(manager);
     }
 
 }
