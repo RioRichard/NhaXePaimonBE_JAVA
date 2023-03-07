00572bfb4ba43c931ac885c086d2eb1d1c748637
RioRichard <68910204+RioRichard@users.noreply.github.com>
2023-03-07 22:51:19 +0700

    Add service get all

---
+++ src/main/java/com/paimon/QLBanVePaimon/services/RoutesService.java
@@ -0,0 +1,24 @@
+package com.paimon.QLBanVePaimon.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+// import com.paimon.QLBanVePaimon.models.Bases;
+import com.paimon.QLBanVePaimon.models.Routes;
+import com.paimon.QLBanVePaimon.repositories.RoutesRepository;
+import com.paimon.QLBanVePaimon.sideModels.ListData;
+
+@Service
+public class RoutesService {
+    
+    @Autowired
+    private RoutesRepository routesRepository;
+
+    public ListData<Routes> getAll(Pageable pageable) {
+
+        var data = routesRepository.findAll(pageable);
+        return new ListData<Routes>(data);
+    }
+}
